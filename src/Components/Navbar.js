e545add761f2e021a8a6ec9ba401b865f26bdfaa
unknown <vladedition.info@gmail.com>
2021-07-26 20:32:52 +0500

    update navbar styling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,20 +1,22 @@
 import {NavLink} from 'react-router-dom'
 
 const Navbar = () => (
-  <nav className='navbar navbar-dark bg-primary navbar-expand-lg mb-3'>
-    <div className='navbar-brand'>Github Search</div>
-    <ul className='navbar-nav'>
-      <li className='nav-item'>
-        <NavLink to='/' exact className='nav-link'>
-          Main Page
-        </NavLink>
-      </li>
-      <li className='nav-item'>
-        <NavLink to='/about' className='nav-link'>
-          Info
-        </NavLink>
-      </li>
-    </ul>
+  <nav className='navbar navbar-dark bg-dark navbar-expand mb-3'>
+    <div className='container'>
+      <div className='navbar-brand fs-3'>Github Search</div>
+      <ul className='navbar-nav fs-5'>
+        <li className='nav-item'>
+          <NavLink to='/' exact className='nav-link'>
+            Main Page
+          </NavLink>
+        </li>
+        <li className='nav-item'>
+          <NavLink to='/about' className='nav-link'>
+            Info
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   </nav>
 )
 
