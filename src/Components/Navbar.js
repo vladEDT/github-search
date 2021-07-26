35a474c7989524b51463fb8022b7c99437628e92
unknown <vladedition.info@gmail.com>
2021-07-26 21:52:56 +0500

    finish restyling

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,7 @@
 import {NavLink} from 'react-router-dom'
 
 const Navbar = () => (
-  <nav className='navbar navbar-dark bg-dark navbar-expand mb-3'>
+  <nav className='navbar navbar-dark bg-dark navbar-expand'>
     <div className='container'>
       <div className='navbar-brand fs-3'>Github Search</div>
       <ul className='navbar-nav fs-5'>
